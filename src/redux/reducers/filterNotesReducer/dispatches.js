d83567c7ac6300303eb5fa97109699eb0f3317e6
Aleksandr Platcev <a.platcev@ya.ru>
2023-10-21 16:22:52 +0300

    fix filter dispatch

--- src/redux/reducers/filterNotesReducer/dispatches.js
+++ src/redux/reducers/filterNotesReducer/dispatches.js
@@ -4,26 +4,30 @@ const filterNotesDispatches = {
       type: "SET_FILTER",
     });
   },
+
   FILTER_NOTES(dispatch, inputName, value, value2, notes) {
     let newFilterNotes, inputName2;
-
-    let regexp = new RegExp(value, "gmi");
-    let regexp2 = new RegExp(value2, "gmi");
 
     if (inputName === "price") inputName2 = "title";
     else inputName2 = "price";
 
     if (value !== "" && value2 === "") {
-      newFilterNotes = notes.filter((note) => regexp.test(note[inputName]));
-
+      newFilterNotes = notes.filter((note) => {
+        return note[inputName].toLowerCase().includes(value.toLowerCase());
+      });
     } else if (value === "" && value2 !== "") {
-      newFilterNotes = notes.filter((note) => regexp2.test(note[inputName2]));
-
-    } else if (value !== "" && value2 !== "") {
-      newFilterNotes = notes.filter(
-        (note) => regexp.test(note[inputName]) && regexp2.test(note[inputName2])
+      newFilterNotes = notes.filter((note) =>
+        note[inputName2].toLowerCase().includes(value2.toLowerCase())
       );
-      
+    } else if (value !== "" && value2 !== "") {
+      newFilterNotes = notes.filter((note) => {
+        if (
+          note[inputName].toLowerCase().includes(value.toLowerCase()) &&
+          note[inputName2].toLowerCase().includes(value2.toLowerCase())
+        ) {
+          return note;
+        }
+      });
     } else {
       return this.SET_FILTER(dispatch);
     }
